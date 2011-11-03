9a4b69cdf9e38faf69f926cb6b34319c9d8a7612
David Trott <github@davidtrott.com>
2011-11-03 16:31:21 -0700

    Updated start.sh to remove internal details.

--- src/main/resources/start.sh
+++ src/main/resources/start.sh
@@ -5,7 +5,7 @@ OS=`uname`
 APP_HOME=`dirname "$(cd "${0%/*}" 2>/dev/null; echo "$PWD"/"${0##*/}")"`
 parentdir=`dirname "$(cd "${abspath}/../" 2>/dev/null; echo "$PWD"/"${0##*/}")"`
 
-GLOBAL_PROPERTIES="/deployments/edmunds/properties/common/${project.artifactId}-startup.properties"
+GLOBAL_PROPERTIES="/deployments/edmunds/properties/common/etm-agent-startup.properties"
 
 if [ -e ${APP_HOME}/startup.properties ]; then
    . ${APP_HOME}/startup.properties
@@ -16,13 +16,11 @@ LOGFILE_NAME="etm-agent.log"
 if [ "${OS}" = "Darwin" ]; then
     echo "Assuming developer mode setting environment variables accordingly."
     CHECK=""
-    DEFAULT_JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home"
     APP_HOME=`pwd`
     DEFAULT_OUTPUT_DIR="${APP_HOME}"
     DEFAULT_MIN_MEM="64m"
     DEFAULT_MAX_MEM="64m"
 else
-    DEFAULT_JAVA_HOME="/apps/sun-jdk-1.6"
     DEFAULT_OUTPUT_DIR="/logs/etm-agent/`hostname -s`"
     DEFAULT_MIN_MEM="64m"
     DEFAULT_MAX_MEM="128m"
@@ -73,10 +71,6 @@ if [ "x${MIN_MEM}" = "x" ]; then
    MIN_MEM=${DEFAULT_MIN_MEM}
 fi
 
-if [ "x${JAVA_HOME}" = "x" ]; then
-     JAVA_HOME="${DEFAULT_JAVA_HOME}"
-fi
-
 if [ "x${OUTPUT_DIR}" = "x" ]; then
     OUTPUT_DIR=${DEFAULT_OUTPUT_DIR}
 fi
@@ -86,13 +80,11 @@ if  [ -d ${OUTPUT_DIR} ]; then
 fi
 
 if [ $# -eq $EXPECTED_ARGS ] && [ x"$1" = "x-fg" ]; then
-        ${JAVA_HOME}/bin/java -Xms${MIN_MEM} -Xmx${MAX_MEM} ${GC_OPTIONS} \
+        java -Xms${MIN_MEM} -Xmx${MAX_MEM} ${GC_OPTIONS} \
         -DOUTPUT_DIR=${OUTPUT_DIR} -DLOGFILE_NAME=${LOGFILE_NAME} \
-        -javaagent:${APP_HOME}/lib/jmxrmi-agent.jar -Dedmunds.jmxrmi.agent.port=${JMX_PORT} \
         ${CLASS_NAME}
 else
-        ${COMMAND_PREFIX} ${JAVA_HOME}/bin/java -Xms${MIN_MEM} -Xmx${MAX_MEM} ${GC_OPTIONS} \
-        -DOUTPUT_DIR=${OUTPUT_DIR} -DLOGFILE_NAME=${LOGFILE_NAME} \
-        -javaagent:${APP_HOME}/lib/jmxrmi-agent.jar -Dedmunds.jmxrmi.agent.port=${JMX_PORT} \
+        java -Xms${MIN_MEM} -Xmx${MAX_MEM} ${GC_OPTIONS} \
+        -DOUTPUT_DIR=${OUTPUT_DIR} -DLOGFILE_NAME=${LOGFILE_NAME} 
         ${CLASS_NAME} 2>/dev/null 1>/dev/null &
 fi
